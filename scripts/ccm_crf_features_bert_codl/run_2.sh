fb18fa1e628a77781e70789e80dc0054b8f31d88
codedecde <barunpatra95@gmail.com>
2019-06-26 14:20:00 -0700

    stupid mistake

--- scripts/ccm_crf_features_bert_codl/run_2.sh
+++ scripts/ccm_crf_features_bert_codl/run_2.sh
@@ -1,2 +1,2 @@
 PYTHON=/zfsauton3/home/bpatra/miniconda3/bin/python3.6
-${PYTHON} -m ccm_model.main --config_file training_configs/ccm_crf_features_bert_codl/ner_type_attr.jsonnet --base_dir ./trained_model_outputs/ccm_crf_features_bert_codl --devices 0 --start_index 70 --end_index 105
+${PYTHON} -m ccm_model.codl_main --config_file training_configs/ccm_crf_features_bert_codl/ner_type_attr.jsonnet --base_dir ./trained_model_outputs/ccm_crf_features_bert_codl --devices 0 --start_index 70 --end_index 105
