89a2210563fd901869cf6f321ed851710fece612
codedecde <barunpatra95@gmail.com>
2019-06-26 13:57:43 -0700

    work in progress

---
+++ scripts/ccm_crf_features_bert_codl/run_2.sh
@@ -0,0 +1,2 @@
+PYTHON=/zfsauton3/home/bpatra/miniconda3/bin/python3.6
+${PYTHON} -m ccm_model.main --config_file training_configs/ccm_crf_features_bert_codl/ner_type_attr.jsonnet --base_dir ./trained_model_outputs/ccm_crf_features_bert_codl --devices 0 --start_index 70 --end_index 105
