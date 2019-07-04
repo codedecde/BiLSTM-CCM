531ef696d7534c7581a83270309328c9f83c061f
codedecde <barunpatra95@gmail.com>
2019-07-04 15:26:47 -0700

    added books scripts 2

--- books-scripts/ccm_crf_sent_features_bert_ner_type/run_2.sh
+++ books-scripts/ccm_crf_sent_features_bert_ner_type/run_2.sh
@@ -1,7 +1,7 @@
 MODEL=ccm_crf_sent_features_bert
 CONFIG_FILE=books_training_configs/${MODEL}/ner_type.jsonnet
 OUTPUT_DIR=./books_trained_model_outputs/${MODEL}_ner_type
-START=46
-END=69
+START=62
+END=92
 PYTHON=/zfsauton3/home/bpatra/miniconda3/bin/python3.6
 ${PYTHON} -m ccm_model.main --config_file ${CONFIG_FILE} --base_dir ${OUTPUT_DIR} --devices 0 --start_index ${START} --end_index ${END}
